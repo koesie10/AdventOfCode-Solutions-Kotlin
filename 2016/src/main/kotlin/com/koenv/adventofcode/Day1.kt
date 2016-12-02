48e861d78f8c91d4424377e1b4a9789eaaaffec0
Koen Vlaswinkel <koen@vlaswinkel.info>
2016-12-02 10:08:24 +0100

    Add Day 1 Part 1

--- 2016/src/main/kotlin/com/koenv/adventofcode/Day1.kt
+++ 2016/src/main/kotlin/com/koenv/adventofcode/Day1.kt
@@ -1,5 +1,54 @@
 package com.koenv.adventofcode
 
 object Day1 {
-    
+    fun getBlocksAway(input: String): Int {
+        var heading: Int = 0 // heading North, positive is clockwise
+        // store our coordinates
+        var x: Int = 0
+        var y: Int = 0
+
+        input.split(",").map(String::trim).forEach {
+            heading = getNewHeading(heading, it[0])
+
+            val blocks = it.substring(1).toInt()
+
+            val (xDiff, yDiff) = getXYDiff(heading, blocks)
+
+            x += xDiff
+            y += yDiff
+        }
+
+        // the total distance is away is just the total x + total y distance
+        return Math.abs(x) + Math.abs(y)
+    }
+
+    fun getNewHeading(oldHeading: Int, direction: Char): Int {
+        var newHeading = oldHeading
+
+        newHeading += when (direction) {
+            'R' -> 90
+            'L' -> -90
+            else -> throw IllegalStateException("Direction is $direction")
+        }
+
+        // make sure we are in the range -360 to 360
+        newHeading = newHeading.mod(360)
+
+        // and we don't want negative headings
+        if (newHeading < 0) {
+            newHeading += 360
+        }
+
+        return newHeading
+    }
+
+    fun getXYDiff(heading: Int, blocks: Int): Pair<Int, Int> {
+        when (heading) {
+            0 -> return 0 to -blocks
+            90 -> return blocks to 0
+            180 -> return 0 to blocks
+            270 -> return -blocks to 0
+            else -> throw IllegalStateException("Heading is $heading")
+        }
+    }
 }
--- 2016/src/test/kotlin/com/koenv/adventofcode/Day1Test.kt
+++ 2016/src/test/kotlin/com/koenv/adventofcode/Day1Test.kt
@@ -1,5 +1,26 @@
 package com.koenv.adventofcode
 
+import org.junit.Test
+import kotlin.test.assertEquals
+
 class Day1Test {
-    
+    @Test
+    fun example1() {
+        assertEquals(5, Day1.getBlocksAway("R2, L3"))
+    }
+
+    @Test
+    fun example2() {
+        assertEquals(2, Day1.getBlocksAway("R2, R2, R2"))
+    }
+
+    @Test
+    fun example3() {
+        assertEquals(12, Day1.getBlocksAway("R5, L5, R5, R3"))
+    }
+
+    @Test
+    fun realInput() {
+        assertEquals(307, Day1.getBlocksAway(getInput(1)))
+    }
 }
